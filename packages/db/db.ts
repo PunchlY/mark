b9da7ed6893e7b7c7ad128d6670a59730e6c8d09
PunchlY <punchly@foxmail.com>
2024-09-07 08:24:10 +0800

    -

--- packages/db/db.ts
+++ packages/db/db.ts
@@ -4,8 +4,8 @@ import migrations from './migrations.sql' with { type: 'text' };
 function GetDatabase(filename: string) {
     const db = new Database(filename, { strict: true });
     db.fileControl(constants.SQLITE_FCNTL_PERSIST_WAL, 0);
-    db.run('PRAGMA JOURNAL_MODE = WAL;');
-    db.run('PRAGMA FOREIGN_KEYS = ON;');
+    db.run('PRAGMA JOURNAL_MODE = WAL');
+    db.run('PRAGMA FOREIGN_KEYS = ON');
 
     db.run(migrations);
 
@@ -16,12 +16,6 @@ const db: Database = process.env.NODE_ENV === 'production' ?
     GetDatabase(Bun.env.DATABASE || 'sqlite.db') :
     // @ts-ignore
     globalThis['$sqlite'] ??= GetDatabase(`${__dirname}/dev.db`);
-
-db.fileControl(constants.SQLITE_FCNTL_PERSIST_WAL, 0);
-db.run('PRAGMA JOURNAL_MODE = WAL;');
-db.run('PRAGMA FOREIGN_KEYS = ON;');
-
-db.run(migrations);
 
 export default db;
 export { GetDatabase };
